7b73451d5ac4ad91c9bc9ef1967afbdaa16d0951
Sarah Clark <sarahireneclark@gmail.com>
2026-02-13 16:11:51 -0600

    Initial commit of Xcode project

---
+++ RecipesTests/RecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesTests.swift
+//  RecipesTests
+//
+//  Created by Sarah Clark on 2/13/26.
+//
+
+import Testing
+@testable import Recipes
+
+struct RecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
